821e031ed9cef4b507e0db2a7e82b877ae930ecb
yannickbaze <yannickbaze392@gmail.com>
2026-08-08 11:55:40 +0200

    routing-css-module

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,21 +1,21 @@
-import {NavLink} from 'react-router-dom'
-function PageNav()
-{
-    return (
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/pricing">Pricing</NavLink>
-          </li>
-          <li>
-            <NavLink to="/product">Product</NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default PageNav
+export default PageNav;
