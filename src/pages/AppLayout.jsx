1e5980b9628cfd7f708ba8f7938cd216a1423e59
yannickbaze <yannickbaze392@gmail.com>
2026-08-09 15:55:05 +0200

    App Layout structure

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import AppNav from '../components/AppNav'
-function AppLayout()
-{
-    return (
-        <div>
-            <AppNav /> 
-            <p>App</p>
-        </div>
-    )
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout
