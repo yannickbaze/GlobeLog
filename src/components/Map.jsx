abff45b650c6a785b20a2eae5b9e494fed45b06b
yannickbaze <yannickbaze392@gmail.com>
2026-08-12 01:46:05 +0200

    Reading and Setting a Query String

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,12 @@
-import styles from './Map.module.css'
-function Map()
-{
-    return (
-        <div className={ styles.mapContainer }>
-            
-        </div>
-    )
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return <div className={styles.mapContainer}>{}</div>;
 }
 
-export default Map
+export default Map;
